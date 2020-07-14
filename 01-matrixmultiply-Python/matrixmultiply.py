9ebd7963f921f9078d8488d21f220c8c9764c474
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-14 16:50:43 +0530

    Jul 14  16:50:42

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,8 +5,11 @@
 
 
 def fun_matrixmultiply(m1, m2):
+
+    if len(m2) == len(m1):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
+                    m[i][j] += m1[i][k] * m2[k][j]
+        return m
     return None
-
-
-
-
