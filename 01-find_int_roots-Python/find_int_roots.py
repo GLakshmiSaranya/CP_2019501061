82ee27129418d7e2e9eb497e8a5748fc62f0ef00
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-14 16:17:18 +0530

    Jul 14  16:17:18

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -8,6 +8,9 @@
 
 import math
 def fun_find_int_roots(a, b, c):
-	return 0, 0
+	d = (b ** 2) - (4 * a *c)
 
+	root1 = ((-b) + sqrt(d) / (2 * a))
+	root2 = ((-b) - sqrt(d) / (2 * a))
 
+	return root1, root2
