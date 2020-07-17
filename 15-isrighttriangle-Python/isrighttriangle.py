a145d7a9b2d2f459ee891bb5bfeae2710fdb4e6c
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-17 12:21:13 +0530

    Jul 17  12:21:13

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -20,4 +20,4 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	pass
 
 def distance(x1, x2, y1, y2):
-	return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+	return (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
