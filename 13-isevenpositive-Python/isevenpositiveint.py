f595857e60193700e9237869b20969a68cb347cd
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-17 11:43:06 +0530

    Jul 17  11:43:06

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -6,8 +6,9 @@
 
 def isevenpositiveint(x):
 	# your code goes here
-	if isinstance(x, int):
-		if x > 0 and x % 2 == 0:
-			return True
+	if type(x) == int:
+		if x > 0:
+			if x % 2 == 0:
+				return True
 	return False
 	pass
