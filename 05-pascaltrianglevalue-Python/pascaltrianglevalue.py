1ad4f5e29044377fdd13e035c22c5a205c807e5c
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-17 15:39:33 +0530

    Jul 17  15:39:32

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -5,8 +5,20 @@
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
 
-
-
-
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	if row < col:
+		return 0
+	if col == 0 or row == col:
+		return 1
+	
+	n = factorial(row)
+	d = factorial(col) * factorial(row - col)
+
+	# value = n!/(r!(n-r)!)
+	res = n // d
+	return res
+	
+def factorial(n):
+	if n == 1:
+		return 1
+	return n * factorial(n - 1)
