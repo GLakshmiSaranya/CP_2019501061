10626d1a6a4ee8dc325925834f19aa0150149c9e
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-17 12:53:21 +0530

    Jul 17  12:53:20

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -7,6 +7,6 @@
 def trianglearea(s1, s2, s3):
 	# your code goes here
 	S = (s1 + s2 + s3) / 2
-	area = (S * (S - 1) * (S - 2) * (S - 3)) ** 2
+	area = (S * (S - s1) * (S - s2) * (S - s3)) ** 0.5
 	return area
 	pass
