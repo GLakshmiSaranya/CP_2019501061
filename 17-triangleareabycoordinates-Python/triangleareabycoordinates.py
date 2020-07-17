f32dd132274abc06d259f9aba663a008068516ad
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-17 12:57:46 +0530

    Jul 17  12:57:45

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,7 +6,15 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
+	X = distance(x1, x2, y1, y2)
+	Y = distance(x2, x3, y2, y3)
+	Z = distance(x1, x3, y1, y3)
+	
+	return trianglearea(X, Y, Z)
 	pass
+
+def distance(x1, x2, y1, y2):
+	return ((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def trianglearea(s1, s2, s3):
 	S = (s1 + s2 + s3) / 2
