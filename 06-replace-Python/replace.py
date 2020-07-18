92518f1795c0d6621ef3a9b5467a6f4717509199
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-18 15:05:38 +0530

    Jul 18  15:05:38

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -3,7 +3,14 @@
 # replace(s1, s2, s3) that returns a string equal to 
 # s1.replace(s2, s3), but again without calling s.replace().
 
-
 def fun_replace(s1, s2, s3):
-	return s1
+	s = ''
 
+	for i in range(0, len(s1)):
+		if (s2 == s1 [i: i + len(s2)]):
+			s += s3
+			i += len(s2)
+		else:
+			s += s1[i]
+			i += 1
+	return s
