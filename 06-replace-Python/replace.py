a817cc6ea7ef382b05ce00c31f667452106ed841
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-18 15:07:47 +0530

    Jul 18  15:07:47

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -4,12 +4,15 @@
 # s1.replace(s2, s3), but again without calling s.replace().
 
 def fun_replace(s1, s2, s3):
+	l1 = len(s1)
+	l2 = len(s2)
 	s = ''
 
-	for i in range(0, len(s1)):
-		if (s2 == s1 [i: i + len(s2)]):
+	i = 0
+	while i < l1:
+		if (s2 == s1 [i: i + l2]):
 			s += s3
-			i += len(s2)
+			i += l2
 		else:
 			s += s1[i]
 			i += 1
