bb69803e4e60f5a16d8a14c1acb1e4bba7208e25
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-22 17:51:10 +0530

    Jul 22  17:51:10

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -3,25 +3,19 @@
 # positive powers of 3 up to and including n. As an example, powersOf3ToN(10.5) returns [1, 3, 9]. If no such powers 
 # of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
 
-power_arr = []
-
 def recursion_powersof3ton(n):
 	# Your code goes here
 	if n < 1:
-		if len(power_arr) == 0:
-			return None
-		arr = power_arr.sort()
-		power_arr.clear()
-		return arr
-
-	if divisible_by_3(n):
-		power_arr.append(n)
-	return recursion_powersof3ton(round(n))
+		return None
+	power_arr = []
+	return divisible_by_3(round(n), power_arr, 0)
 	pass
 
-def divisible_by_3(n):
-	if n == 1 or n == 3:
-		return True
-	elif n % 3 > 0:
-		return False
-	return divisible_by_3(n / 3)
+def divisible_by_3(num, power_arr, n):
+	if num <= n:
+		return power_arr
+	val = 3 ** n 
+	if val < num:
+		power_arr.append(val)
+	n += 1
+	return divisible_by_3(num, power_arr, n)
