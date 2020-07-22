2de03b10623f521f0260f2dc28d224e09d14b747
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-22 12:40:15 +0530

    Jul 22  12:40:15

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,12 +8,13 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-even_arr = []
 def fun_recursion_onlyevendigits(L):
 	l = len(L)
+	even_arr = []
 
 	if l == 0:
 		return even_arr
+	
 	even_arr.append(only_even_digits(L.pop(0), 0))
 	
 	for i in range(l):
