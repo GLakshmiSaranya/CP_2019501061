0837582cf9674e5fe61bcbb5f848921560b42b05
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-24 18:02:03 +0530

    Jul 24  18:02:03

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,36 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+
 def ishappyprimenumber(n):
     # Your code goes here
+    res = 0
+    count = 0
+    
+    while count <= n:
+        res += 1
+        if ishappynumber(res) and isPrime(res):
+            count += 1
+            
+    return res
     pass
+
+def ishappynumber(n):
+	if n == 1 or n == 7:
+		return True
+	sum = n
+	while sum > 9:
+		sum = 0
+		while n > 0:
+			sum += (n % 10) ** 2
+			n //= 10
+		if sum == 1:
+			return True
+		n = sum
+	return False
+
+def isPrime(n):
+	for i in range(2, (n // 2) + 1):
+		if n % i == 0:
+			return False
+	return True
