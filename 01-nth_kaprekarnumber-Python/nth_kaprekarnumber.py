83155f48bb16138b3d2fa6fc65127b8791d6864f
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-24 17:36:53 +0530

    Jul 24  17:36:53

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -6,8 +6,28 @@
 # With this in mind, write the function nthKaprekarNumber(n) that takes a non-negative int n 
 # and returns the nth Kaprekar number, where as usual we start counting at n==0.
 
-
 import math
 
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    count = 0
+    k_num = 1
+    i = 1
+
+    while count <= n:
+        if is_kaprekarnumber(i):
+            count += 1
+            k_num = i
+        i += 1
+    return k_num
+
+def is_kaprekarnumber(n):
+    sqr = str(n ** 2)
+    l = len(sqr)
+
+    for i in range(1, l):
+        s1 = int("".join(sqr[ : i]))
+        s2 = int("".join(sqr[i : ]))
+
+        if s1 + s2 == n:
+            return True
+    return False
