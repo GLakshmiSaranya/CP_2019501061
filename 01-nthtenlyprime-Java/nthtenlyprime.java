db70e83fb5bf22ec918f0d1ecc03d78c4e6c697c
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-07-28 20:36:30 +0530

    Jul 28  20:36:30

--- 01-nthtenlyprime-Java/nthtenlyprime.java
+++ 01-nthtenlyprime-Java/nthtenlyprime.java
@@ -1,11 +1,50 @@
-// # Write the function fun_nth_additive_prime(n) that takes a non-negative int n 
-// # and returns the nth Additive Prime, which is a prime number such that 
-// # the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
-// # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
-
+// nthTenlyPrime(n)
+// We will say that a number is "tenly" (a made-up term) if the digits of the 
+// number add up to 10. So 1153 is tenly, but 153 is not. With this in mind, write 
+// the function nthTenlyPrime(n) that takes a non-negative int n and returns the 
+// nth number that is both tenly and prime. You should also write all the required 
+// helper functions. The first several tenly primes are: 19, 37, 73, 109, 127…
 
 class nthtenlyprime {
-	public int fun_nthtenlyprime(int n){
-		return 1;
-	}
+	public int fun_nthtenlyprime(int n) {
+        int count = 0;
+        int tenlyPrime = 19;
+
+        while (count <= n) {
+            tenlyPrime += 2;
+
+            if (sumOfDigits(tenlyPrime) == 10 && isPrime(tenlyPrime)) {
+                count++;
+            }
+        }
+		return tenlyPrime;
+    }
+    
+    public int sumOfDigits(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        if (n == 2 || n == 3) {
+            return true;
+        }
+
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
